dcc35fde0a11c4184e71aab82fea61d1ddde6fb6
victormelos <victormelotrabalho1@gmail.com>
2025-04-16 20:18:34 -0300

    commit
       git push -u origin main#

--- src/components/CheckersBoard/CheckersBoard.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Dummycomponent = () => {
-  return <div />;
-};
-
-export default Dummycomponent;
--- src/components/home.tsx
+++
@@ -1,8 +0,0 @@
-function Home() {
-  return (
-    <div className="w-screen h-screen">
-    </div>
-  )
-}
-
-export default Home
